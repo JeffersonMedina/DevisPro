979562e4a0c4c0e81f6930c953225b6b9cb12c8c
crixo911 <crixo_911@yahoo.es>
2018-04-06 19:09:15 -0500

    login

--- DebisPro_App/DebisPro_App/Properties/Resources.Designer.cs
+++ DebisPro_App/DebisPro_App/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace DebisPro_App.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap LogoDevis_pro {
+            get {
+                object obj = ResourceManager.GetObject("LogoDevis-pro", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
